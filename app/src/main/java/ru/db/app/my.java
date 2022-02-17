0a3bc614e8453df0068a9f8c6f98fc3d0c2463d4
ben <ben.tn2002@mail.ru>
2022-02-17 22:26:12 +0300

    .

--- app/src/main/java/ru/db/app/my.java
+++ app/src/main/java/ru/db/app/my.java
@@ -1,8 +1,15 @@
 package ru.db.app;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.UnsupportedEncodingException;
@@ -69,5 +76,24 @@ public class my {
             e.printStackTrace();
         }
         return "";
+    }
+
+    private void getListItems(String phone) {
+        DocumentReference docRef = db.collection("cities").document("SF");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        document.getData();
+                    } else {
+                        // Not found
+                    }
+                } else {
+                    // Can'nt find
+                }
+            }
+        });
     }
 }

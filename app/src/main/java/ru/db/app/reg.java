6d1af59ae8f480624f50e71831e7d95c8376ce5b
Мардон Жумонов <smaricpb@gmail.com>
2022-02-17 21:43:52 +0300

    s

--- app/src/main/java/ru/db/app/reg.java
+++ app/src/main/java/ru/db/app/reg.java
@@ -2,8 +2,10 @@ package ru.db.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class reg extends AppCompatActivity {
 
@@ -18,6 +20,23 @@ public class reg extends AppCompatActivity {
     }
 
     public void check_on_passenger(View view) {
-        View passenger = findViewById(R.id.passenger_check)
+        TextView passenger_text = findViewById(R.id.passenger_textview);
+        passenger_text.setTextColor(Color.BLACK);
+        View passenger = findViewById(R.id.passenger_check);
+        passenger.setVisibility(View.VISIBLE);
+        View carrier = findViewById(R.id.carrier_check);
+        carrier.setVisibility(View.GONE);
+        TextView carrier_text = findViewById(R.id.carrier_textview);
+        carrier_text.setTextColor(Color.GRAY);
+    }
+    public void check_on_carrier(View view) {
+        TextView passenger_text = findViewById(R.id.passenger_textview);
+        passenger_text.setTextColor(Color.GRAY);
+        View passenger = findViewById(R.id.passenger_check);
+        passenger.setVisibility(View.GONE);
+        View carrier = findViewById(R.id.carrier_check);
+        carrier.setVisibility(View.VISIBLE);
+        TextView carrier_text = findViewById(R.id.carrier_textview);
+        carrier_text.setTextColor(Color.BLACK);
     }
 }
